26be46263cc4a6b2fb3e21bbe42d86f85ad08e71
danilovukosich <vukosichdanilo@gmail.com>
2024-09-30 23:22:49 -0300

    card game finished logic

--- src/app/components/games/mayor-menor/mayor-menor.component.ts
+++ src/app/components/games/mayor-menor/mayor-menor.component.ts
@@ -4,6 +4,7 @@ import { RouterLink } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { CartasService } from '../../../services/cartas.service';
 import { CommonModule } from '@angular/common';
+import { NgToastService } from 'ng-angular-popup';
 
 
 
@@ -16,34 +17,28 @@ import { CommonModule } from '@angular/common';
 })
 export class MayorMenorComponent {
 
+  //METRICAS
   puntos!:number;
   vidas!:number;
 
+  //CARTAS
   mazo!:any;
-  carta!:any;
+  cartaActual!:any;
+  cartaSiguiente!:any;
   cartaImagen!:HTMLImageElement;
 
+  //SUBCRIPTION
   subscripcion!:Subscription;
 
-  constructor(private cartaService:CartasService){}
+  constructor(private cartaService:CartasService, private toast:NgToastService){}
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.vidas = 2;//sacar cunado no se mas de prueba
-    this.puntos = 0;//sacar cunado no se mas de prueba
+    this.vidas = 3;
+    this.puntos = 0;
 
-   /*  this.subscripcion = this.paisesService.getPaises()
-    .subscribe(paises => {
-      this.paises = paises;
-      
-    });
- */
     this.TraerMazo();
-    setTimeout(()=>{
-      this.TraerCarta();
-    },1000);
-
   }
 
   
@@ -51,18 +46,112 @@ export class MayorMenorComponent {
   {
     this.cartaService.GetMazo().subscribe((data)=>{
       this.mazo = data;
+      this.TraerPrimerCarta();
       console.log(this.mazo);
-      console.log(this.mazo.deck_id);
+      
     });
   }
 
-  TraerCarta()
+  TraerPrimerCarta()
   {
     this.cartaService.GetCarta(this.mazo.deck_id).subscribe((carta)=>{
-      this.carta = carta.cards[0];
+      this.cartaActual = carta.cards[0];
       this.cartaImagen = carta.cards[0].image;
-      console.log(this.carta);
+      /* console.log(this.carta); */
     });
+  }
+
+  TraerSiguienteCartaMayor()
+  {
+    this.cartaService.GetCarta(this.mazo.deck_id).subscribe((carta)=>{
+      
+      this.cartaSiguiente = carta.cards[0];
+
+      const valorActual = this.convertirValor(this.cartaActual.value);
+      const valorSiguiente = this.convertirValor(this.cartaSiguiente.value);
+
+      
+      /* console.log(valorSiguiente);
+      console.log("Es mayor que...");
+      console.log(valorActual); */
+
+      if(valorSiguiente >= valorActual)
+        {
+          this.puntos+=5;
+        }
+        else
+        {
+          this.vidas-=1;
+        }
+
+        this.cartaActual=this.cartaSiguiente;
+        this.cartaImagen=this.cartaActual.image;
+
+    });
+  }
+
+  TraerSiguienteCartaMenor()
+  {
+    this.cartaService.GetCarta(this.mazo.deck_id).subscribe((carta)=>{
+      
+      this.cartaSiguiente = carta.cards[0];
+
+      const valorActual = this.convertirValor(this.cartaActual.value);
+      const valorSiguiente = this.convertirValor(this.cartaSiguiente.value);
+
+      /* console.log(valorSiguiente);
+      console.log("Es menor que...");
+      console.log(valorActual); */
+      
+      
+
+      if(valorSiguiente <= valorActual)
+        {
+          this.puntos+=5;
+        }
+        else
+        {
+          this.vidas-=1;
+        }
+
+        this.cartaActual=this.cartaSiguiente;
+        this.cartaImagen=this.cartaActual.image;
+
+    });
+  }
+
+  convertirValor(valor: string): number 
+  {
+    switch (valor) 
+    {
+      case 'ACE':
+        return 14;
+
+      case 'KING':
+        return 13;
+
+      case 'QUEEN':
+        return 12;
+
+      case 'JACK':
+        return 11;
+
+      default:
+        return parseInt(valor);
+    }
+  }
+  
+
+  ngAfterViewChecked()//chekea cambios en la view
+  {
+    if(this.vidas==0)
+    {
+      this.toast.danger("Sus puntos fueron: "+this.puntos, "PERDIO");
+      this.puntos = 0;
+      this.vidas = 3;
+
+      this.TraerMazo();
+    }
   }
 
 
--- src/app/services/cartas.service.ts
+++ src/app/services/cartas.service.ts
@@ -10,7 +10,8 @@ export class CartasService {
 
   constructor(private httpClient: HttpClient) { }
 
-
+  //https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1    mazo normal
+  //https://www.deckofcardsapi.com/api/deck/new/shuffle/?cards=2S,3S,4S,5S,6S,7S,8S,9S,0S,2D,3D,4D,5D,6D,7D,8D,9D,0D,2C,3C,4C,5C,6C,7C,8C,9C,0C,2H,3H,4H,5H,6H,7H,8H,9H,0H     //mazo numerico
   GetMazo():Observable<any>{
     return this.httpClient.get<any>('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
     .pipe(map(cartas=>cartas));
