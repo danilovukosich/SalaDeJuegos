3569ec5c988a8b961f2e2b952da22b3406760583
danilovukosich <vukosichdanilo@gmail.com>
2024-10-01 23:38:02 -0300

    ahorcado keyboard & minor fixes

--- src/app/components/games/ahorcado/ahorcado.component.ts
+++ src/app/components/games/ahorcado/ahorcado.component.ts
@@ -1,13 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-ahorcado',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, FormsModule],
   templateUrl: './ahorcado.component.html',
   styleUrl: './ahorcado.component.css'
 })
 export class AhorcadoComponent {
 
+  vidas!:number;
+  puntos!:number;
+
+  constructor()
+  {
+    this.puntos=10;//cambiar a cero cuando se cree la logica
+  }
 }
